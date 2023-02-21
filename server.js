05b5bd8b4370022120773b05e6832cca223eac43
Mohamed <abdi2k24@gmail.com>
2023-02-21 01:52:04 -0600

    updated bug and server js files

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
+  );
